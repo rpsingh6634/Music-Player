82ea83091403d45112998595bef2ae83dc653b14
rpsingh6634 <rpsingh6634@gmail.com>
2021-09-22 23:21:34 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+let allMusic = [
+  {
+    name: "7-Rings",
+    artist: "Ariana Grande",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Hai Dil Ye Mera",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Bol Kaffara Kya Hoga",
+    artist: "Sehar Gul Khan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Apna Time Aayega",
+    artist: "Divine",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Smack That",
+    artist: "Akon ft.Eminem",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Humnva",
+    artist: "Papon,Mithoon",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Standing In The Rain",
+    artist: "John Paul Young",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "International Love Mashup",
+    artist: "Unknown",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Summertime Sadness",
+    artist: "Lana Del Rey",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Rockstar",
+    artist: "Post Malone",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Stressed Out",
+    artist: "Blurryface",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Car Radio",
+    artist: "Twenty One Pilots",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Heathens",
+    artist: "Suicide Squad: The Album",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Ride",
+    artist: "Blurryface",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Wishlist",
+    artist: "Dino James",
+    img: "music-15",
+    src: "music-15"
+  },
+];
